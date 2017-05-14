b20a37964853de4ea883b33a05356ce67f20eaa2
joakm-bang <jocke.bang@gmail.com>
2017-05-14 20:22:39 +0200

    dist

--- vbox_atg.py
+++ vbox_atg.py
@@ -1,5 +1,5 @@
 import numpy as np
-import psycopg2
+#import psycopg2
 import mechanicalsoup
 from time import sleep, time, ctime
 from datetime import datetime, timedelta
@@ -336,7 +336,27 @@ class Settings:
             self.pdate0 = 0
             self.pdate1 = 734698
             self.game_type = 'vinnare'
-        
+        if self.computer == 'vbox2':
+            self.runLAN = True
+            self.pdate0 = 734698
+            self.pdate1 = 735139
+            self.game_type = 'vinnare'
+        if self.computer == 'vbox3':
+            self.runLAN = True
+            self.pdate0 = 735139
+            self.pdate1 = 735580
+            self.game_type = 'vinnare'
+        if self.computer == 'vbox4':
+            self.runLAN = True
+            self.pdate0 = 735580
+            self.pdate1 = 736021
+            self.game_type = 'vinnare'
+        if self.computer == 'vbox5':
+            self.runLAN = True
+            self.pdate0 = 736021
+            self.pdate1 = 1000000
+            self.game_type = 'vinnare'
+    
         self.configure_db()
 
     def configure_db(self):
