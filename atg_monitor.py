52dc8e8a293bf8d3cbeae96fe6ffb9a3ba10df0c
joakm-bang <jocke.bang@gmail.com>
2017-05-16 20:00:08 +0200

    monitor

--- atg_monitor.py
+++ atg_monitor.py
@@ -1,5 +1,5 @@
 from os import system
-from time import sleep
+from time import sleep, ctime
 from sys import stdout
 
 stdout = open('/home/joakim/work/mlog.log', 'w')
@@ -11,6 +11,7 @@ while True:
     td = int(datetime.timestamp(datetime.now())) - ts
     if td > 600:
         system('sudo reboot')
+    print('System online at {0}, with {1} idle seconds'.format(ctime(), str(td)))
     sleep(30)
     
     
