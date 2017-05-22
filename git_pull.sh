43aea3f3f226c153f864afc2e0fb800199c757ca
joakm-bang <jocke.bang@gmail.com>
2017-05-23 06:25:56 +1000

    fix

--- git_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 2s
-cd /home/joakim/work/horse
-git pull origin master
-exit 0
---
+++ gitpull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 2s
+echo "Running git"
+cd /home/joakim/work/horse
+git pull origin master
+exit 0
