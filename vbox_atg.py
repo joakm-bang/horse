654b5624fc21e6070c85a822655a6fb28f7a11e4
joakm-bang <jocke.bang@gmail.com>
2017-05-25 00:03:29 +0200

    speed

--- vbox_atg.py
+++ vbox_atg.py
@@ -364,11 +364,11 @@ class Settings:
         if self.computer == 'vbox3':
             self.runLAN = True
             self.pdate0 = 734900
-            self.pdate1 = 736021
+            self.pdate1 = 735700
             self.game_type = 'tvilling'
         if self.computer == 'vbox4':
             self.runLAN = True
-            self.pdate0 = 736021
+            self.pdate0 = 735700
             self.pdate1 = 1000000
             self.game_type = 'tvilling'
         if self.computer == 'vbox5':
