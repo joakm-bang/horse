bbc42966b27e6363e0c2ae7f66bb62fd10447da2
joakm-bang <jocke.bang@gmail.com>
2017-05-25 20:39:58 +0200

    speed

--- vbox_atg.py
+++ vbox_atg.py
@@ -213,7 +213,7 @@ class Atg:
     
     def get_games(self):
         
-        qn = 0
+        #qn = 0
         self.Q = RaceQueue()
         while not self.Q.is_empty():
             ID = self.Q.pop()
@@ -223,10 +223,10 @@ class Atg:
             with open(log_file, 'a') as log:
                 log.write('Downloaded {0} at {1}\n'.format(ID, ctime()))
 
-            qn += 1
-            if qn == 100:
-                self.Q.fill()
-                qn = 0
+            #qn += 1
+            #if qn == 100:
+                #self.Q.fill()
+                #qn = 0
             
         
 class CalendarParser:
